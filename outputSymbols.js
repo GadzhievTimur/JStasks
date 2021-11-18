aaed43ebed3c3b7c603f63790dd7aef5bfcb6867
GadzhievTimur <gadzhiev.exceedteam@gmail.com>
2021-11-18 12:12:10 +0300

    1st commit for JStasks

---
+++ outputSymbols.js
@@ -0,0 +1,9 @@
+let str = 'Я изучаю JavaScript';
+
+alert(str[3]); //з
+alert(str[0]); //Я
+alert(str.slice(-1)); //t
+
+alert(str.substr(0, 1)) // Я
+alert(str.substr(3, 1)) // з
+alert(str.substr(-1)) // t
