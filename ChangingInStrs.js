96b183d9da877275caa81d02306578c6bf16108a
GadzhievTimur <94054596+GadzhievTimur@users.noreply.github.com>
2021-11-18 12:47:18 +0300

    Update ChangingInStrs.js

--- ChangingInStrs.js
+++ ChangingInStrs.js
@@ -1,4 +1,4 @@
 let str = 'Я изучаю JavaScript';
 str = str.replace("ю", "л"); //Я изучал JavaScript
-str = str.replace('JavaScript', 'Frontend') // Я изучал Frontend
+str = str.replace('JavaScript', 'Frontend'); // Я изучал Frontend
 alert(str);
